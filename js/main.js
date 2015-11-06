a752c1f753c5429b10fcb6bbacf566855c93ef64
Bryan Bierce <bryanbierce@gmail.com>
2015-11-06 12:02:56 -0800

    Change load process for '.nav-pic' to prevent user witnessing scaling. Removed unneccesary font link.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  var navButtonHeight = $('.button-nav-text').height();
+  var navPic = $('.nav-pic');
+  navPic.css('height', navButtonHeight*2.51);
+  navPic.attr('src', 'assets/blue_bottle_square.jpg');
+});
--- js/preload.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  var navButtonHeight = $('.button-nav-text').height();
-  $('.nav-pic').css('height', navButtonHeight*2.51);
-});
