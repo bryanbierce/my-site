454fbe9de3ad24faf57d401ebc0f6f932d44076f
Bryan Bierce <bryanbierce@gmail.com>
2016-01-01 10:24:44 -0800

    Moved old site into 'original' folder. Preping for rebuild

--- original/js/main.js
+++ original/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   var navButtonHeight = $('.button-nav-text').height();
   var navPic = $('.nav-pic');
+  var navPicBlog = $('.nav-pic-blog');
   navPic.css('height', navButtonHeight*2.51);
   navPic.attr('src', 'assets/blue_bottle_square.jpg');
+  navPicBlog.css('height', navButtonHeight*2.51);
+  navPicBlog.attr('src', '../assets/blue_bottle_square.jpg');
 });
